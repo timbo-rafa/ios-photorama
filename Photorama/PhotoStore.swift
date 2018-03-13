2d9d4ae5f7178e327f1baf4f0999b583487aac97
Student <student@setass-mac-mini-20.local>
2018-03-12 20:35:07 -0400

    parsing JSON data

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -1,27 +1,33 @@
 import Foundation
 
+enum PhotosResult {
+    case success([Photo])
+    case failure(Error)
+}
+
 class PhotoStore {
     private let session: URLSession = { () -> URLSession in
         let config = URLSessionConfiguration.default
         return URLSession(configuration: config)
     }()
     
-    func fetchInterestingPhotos() {
+    func fetchInterestingPhotos(completion: @escaping (PhotosResult) -> Void) {
         let url = FlickrAPI.interestingPhotosURL
         let request = URLRequest(url: url)
         let task = session.dataTask(with: request) {
             (data, response, error) -> Void in
             
-            if let jsonData = data {
-                if let jsonString = String(data: jsonData, encoding: .utf8) {
-                    print(jsonString)
-                }
-            } else if let requestError = error {
-                print("Error fetching interesting photos: \(requestError)")
-            } else {
-                print("Unexpected error with the request")
-            }
+            let result = self.processPhotosRequest(data: data, error: error)
+            completion(result)
         }
         task.resume()
+    }
+    
+    private func processPhotosRequest(data: Data?, error: Error?) -> PhotosResult {
+        guard let jsonData = data else {
+            return .failure(error!)
+        }
+        
+        return FlickrAPI.photos(fromJSON: jsonData)
     }
 }
--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -7,6 +7,15 @@ class PhotosViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        store.fetchInterestingPhotos()
+        store.fetchInterestingPhotos {
+            (PhotosResult) -> Void in
+            
+            switch PhotosResult {
+            case let .success(photos):
+                print("Successfully found \(photos.count) photos.")
+            case let .failure(error):
+                print("Error fetching interesting photos: \(error)")
+            }
+        }
     }
 }
