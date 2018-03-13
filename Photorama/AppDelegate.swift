3a8afb5a98054f7070ceba6750f9c5788227279c
Student <student@setass-mac-mini-20.local>
2018-03-12 20:03:03 -0400

    property injection at AppDelegate

--- Photorama/AppDelegate.swift
+++ Photorama/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let rootViewController = window!.rootViewController as! UINavigationController
+        let photoViewController = rootViewController.topViewController as! PhotosViewController
+        photosViewController.store = PhotoStore()
         return true
     }
 
