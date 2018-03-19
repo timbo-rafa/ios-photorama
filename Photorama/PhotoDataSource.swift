97f19f57027b9766593e7dced2a774b4c54cb961
Student <student@setass-mac-mini-20.local>
2018-03-19 19:20:47 -0400

    Collection View Grid

---
+++ Photorama/PhotoDataSource.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    var photos = [Photo]()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "UICollectionViewCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        return cell
+    }
+}
--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -1,11 +1,16 @@
 import UIKit
 
 class PhotosViewController: UIViewController {
-    @IBOutlet var imageView: UIImageView!
     var store: PhotoStore!
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    let photoDataSource = PhotoDataSource()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = photoDataSource
         
         store.fetchInterestingPhotos {
             (PhotosResult) -> Void in
@@ -13,25 +18,14 @@ class PhotosViewController: UIViewController {
             switch PhotosResult {
             case let .success(photos):
                 print("Successfully found \(photos.count) photos.")
-                if let firstPhoto = photos.first {
-                    self.updateImageView(for: firstPhoto)
-                }
+                self.photoDataSource.photos = photos
             case let .failure(error):
                 print("Error fetching interesting photos: \(error)")
+                self.photoDataSource.photos.removeAll()
             }
+            self.collectionView.reloadSections(IndexSet(integer: 0))
         }
     }
     
-    func updateImageView(for photo: Photo) {
-        store.fetchImage(for: photo) {
-            (imageResult) -> Void in
-            
-            switch imageResult {
-            case let .success(image):
-                self.imageView.image = image
-            case let .failure(error):
-                print("Error downloading image \(error)")
-            }
-        }
-    }
+    
 }
